ec4acdb86784b5ac23d96cedb26d9c8fb95e0edb
IPeCompany <ma.sehhat@gmail.com>
2019-04-27 17:41:24 +0430

    continous-contacts

--- src/app.routes.ts
+++ src/app.routes.ts
@@ -4,9 +4,17 @@ import {DashboardComponent} from './app/dashboard/dashboard.component';
 import {RegisterComponent} from './app/register/register.component';
 import {CanActivateRouteGuard} from './app/shared/CanActivateRouteGuard';
 import {ForgotPasswordComponent} from './app/forgot-password/forgot-password.component';
+import {GroupComponent} from './app/group/group.component';
+import {GroupListComponent} from './app/group/group-list/group-list.component';
+import {ContactComponent} from './app/group/contact/contact.component';
+import {AddContactComponent} from './app/group/add-contact/add-contact.component';
 
 export const APP_ROUTES: Routes = [
   {path: '', component: DashboardComponent,canActivate:[CanActivateRouteGuard]},
+  {path: 'group/:id',component: GroupComponent},
+  {path: 'group',component: GroupListComponent},
+  {path: 'group/:id/contact/:id',component: ContactComponent},
+  {path: 'group/:id/add-contact',component: AddContactComponent,},
   {path: 'login', component: LoginComponent },
   {path: 'register', component: RegisterComponent},
   {path: 'forgot-password', component: ForgotPasswordComponent},
--- src/app/group/group.component.ts
+++ src/app/group/group.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {GroupService} from './group.service';
+import {ActivatedRoute, Router} from '@angular/router';
+import {assertNumber} from '@angular/core/src/render3/assert';
 
 @Component({
   selector: 'app-group',
@@ -7,9 +10,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GroupComponent implements OnInit {
 
-  constructor() { }
+  groups:any;
+  id:string;
+
+  constructor(private groupService:GroupService,
+              private activatedRoute:ActivatedRoute) { }
 
   ngOnInit() {
+    this.id = this.activatedRoute.snapshot.paramMap.get('id');
+      this.groups = this.groupService.getGroup(this.id);
   }
-
 }
