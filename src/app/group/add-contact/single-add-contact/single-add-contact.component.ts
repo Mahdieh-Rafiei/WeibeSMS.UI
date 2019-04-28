8fd53f12ef7b5cf7ed4e665f2c2253c5fce2bf3d
mahdiyee <rafiee.mahdiye@gmail.com>
2019-04-28 12:15:42 +0430

    create contact

---
+++ src/app/group/add-contact/single-add-contact/single-add-contact.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-single-add-contact',
+  templateUrl: './single-add-contact.component.html',
+  styleUrls: ['./single-add-contact.component.css']
+})
+export class SingleAddContactComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
