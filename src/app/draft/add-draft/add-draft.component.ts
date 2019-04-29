4784d80d1864209c3e782e31cae8db8fa8a2ac6c
mahdiyee <rafiee.mahdiye@gmail.com>
2019-04-29 15:08:34 +0430

    complete draft

---
+++ src/app/draft/add-draft/add-draft.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-draft',
+  templateUrl: './add-draft.component.html',
+  styleUrls: ['./add-draft.component.css']
+})
+export class AddDraftComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
