8c9bd2c8626888a268ffce2f8df442f503938017
IPeCompany <ma.sehhat@gmail.com>
2019-05-08 17:12:37 +0430

    ending user notifications

--- src/app/shared/config.service.ts
+++ src/app/shared/config.service.ts
@@ -16,8 +16,8 @@ export class ConfigService {
   constructor() {
     this.tokenKeyName='jwt-sms';
 
-    // this.baseUrl = 'http://185.211.58.52/appapi/v1/';
     // this.baseUrl = 'http://localhost:60241/appapi/v1/';
+    // this.baseUrl = 'http://185.211.58.52/appapi/v1/';
     this.baseUrl = 'http://192.168.1.94:8070/appapi/v1/';
 
     this.errorMessages.set(1,'Entered values are wrong!');
