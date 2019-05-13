cb125c8192a9be9b18eb5ee7ebf783c28fc6e6d1
mahdiyee <rafiee.mahdiye@gmail.com>
2019-05-13 15:13:49 +0430

    create components

---
+++ src/app/user-account/privacy/deactive-account/deactive-account.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-deactive-account',
+  templateUrl: './deactive-account.component.html',
+  styleUrls: ['./deactive-account.component.css']
+})
+export class DeactiveAccountComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/user-account/reward-point/reward-point.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reward-point',
+  templateUrl: './reward-point.component.html',
+  styleUrls: ['./reward-point.component.css']
+})
+export class RewardPointComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
