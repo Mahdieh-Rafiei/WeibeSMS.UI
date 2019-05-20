9f4b5549e6270c9fa93c992ed947c2f4704c7c2f
mahdiyee <rafiee.mahdiye@gmail.com>
2019-05-20 12:32:15 +0430

    change billing

--- src/app/main/pages/biling/create-transaction/create-transaction.component.ts
+++ src/app/main/pages/biling/create-transaction/create-transaction.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-create-transaction',
   templateUrl: './create-transaction.component.html',
-  styleUrls: ['./create-transaction.component.css']
+  styleUrls: ['./create-transaction.component.scss']
 })
 export class CreateTransactionComponent implements OnInit {
 
