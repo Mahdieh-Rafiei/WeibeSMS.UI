316e75f72bd1297b7233b9fdf0cefb73770d37db
masehhat <ma.sehhat@gmail.com>
2019-05-29 17:06:53 +0430

    fix

--- src/app/shared/module/shared.module.ts
+++ src/app/shared/module/shared.module.ts
@@ -4,35 +4,38 @@ import {CommonModule} from '@angular/common';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {MatCoreModule} from './mat-core-module';
 import {ImageUploadComponent} from '../component/image-upload/image-upload.component';
-import {SearchComponent} from "../component/search/search.component";
-import {NgxPaginationModule} from "ngx-pagination";
-import {DateTimePipe} from "../pipe/date-time.pipe";
+import {SearchComponent} from '../component/search/search.component';
+import {NgxPaginationModule} from 'ngx-pagination';
+import {DateTimePipe} from '../pipe/date-time.pipe';
+import {FilterComponent} from '../component/filter/filter.component';
 
 @NgModule({
-    imports: [
-        CommonModule,
-        FormsModule,
-        ReactiveFormsModule,
-        MatCoreModule,
-        NgxPaginationModule,
-    ],
-    declarations: [
-        CountDownComponent,
-        ImageUploadComponent,
-        SearchComponent,
-        DateTimePipe,
-    ],
-    exports: [
-        CommonModule,
-        FormsModule,
-        ReactiveFormsModule,
-        MatCoreModule,
-        NgxPaginationModule,
-        CountDownComponent,
-        ImageUploadComponent,
-        SearchComponent,
-        DateTimePipe,
-    ]
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatCoreModule,
+    NgxPaginationModule,
+  ],
+  declarations: [
+    CountDownComponent,
+    ImageUploadComponent,
+    SearchComponent,
+    DateTimePipe,
+    FilterComponent
+  ],
+  exports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatCoreModule,
+    NgxPaginationModule,
+    CountDownComponent,
+    ImageUploadComponent,
+    SearchComponent,
+    DateTimePipe,
+    FilterComponent
+  ]
 })
 export class SharedModule {
 }
