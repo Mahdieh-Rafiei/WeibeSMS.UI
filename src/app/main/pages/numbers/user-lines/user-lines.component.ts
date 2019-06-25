44a0998fb2b10bf95c038fbe8799e38778e751b4
mahdiyee <rafiee.mahdiye@gmail.com>
2019-06-25 18:29:54 +0430

    lines table0

--- src/app/main/pages/numbers/user-lines/user-lines.component.ts
+++ src/app/main/pages/numbers/user-lines/user-lines.component.ts
@@ -5,77 +5,93 @@ import {GetUserLineModelInterface} from './models/get-user-line-model.interface'
 import {NotificationService} from '../../../../shared/notification.service';
 import {MatDialog} from '@angular/material';
 import {EditUserLinesComponent} from './edit-user-lines/edit-user-lines.component';
+import {TableConfigInterface} from "../../../../shared/component/table/models/table-config.interface";
+import {PagingModel} from "../../../../shared/component/table/models/paging-model";
 
 @Component({
-  selector: 'app-buy-numbers-list',
-  templateUrl: './user-lines.component.html',
-  styleUrls: ['./user-lines.scss']
+    selector: 'app-buy-numbers-list',
+    templateUrl: './user-lines.component.html',
+    styleUrls: ['./user-lines.scss']
 })
 
 export class UserLinesComponent implements OnInit {
+    data: any;
 
-  getUserLineModel: GetUserLineModelInterface;
-  userLines: ItemsUserLineInterface[];
-  totalItemsCount;
-  currentIndex: number;
+    userLines: ItemsUserLineInterface[] = [];
+    phrase = '';
 
-  phrase = '';
+    filterData = {};
 
-  constructor(private numberService: NumbersService,
-              private notificationService: NotificationService,
-              private dialog: MatDialog) {
-  }
+    tableConfig: TableConfigInterface = {
+        hasManageColumn: true,
+        rowColumnsConfig: [],
+        pagingModel: new PagingModel(),
+        headerNames: ['Id', 'Country', 'Number', 'Next billing date', 'Cost','Manage']
+    };
 
-  ngOnInit() {
-    this.numberService.mode = 'yourNumbers';
-    this.getUserLineModel = {pageNumber: 1, pageSize: 10, isActive: null};
-    this.getUserLines();
-  }
+    getUserLineModel: GetUserLineModelInterface={
+      isActive:null
+    };
 
-  getUserLines() {
-    this.numberService.getUserLines(this.getUserLineModel.pageNumber, this.getUserLineModel.pageSize, this.getUserLineModel.isActive, this.phrase)
-      .subscribe(res => {
-        this.userLines = res.data.items;
-        this.totalItemsCount = res.data.totalItemsCount;
-        console.log(this.userLines);
-      });
-  }
+    currentIndex: number;
+
+    constructor(private numberService: NumbersService,
+                private notificationService: NotificationService,
+                private dialog: MatDialog) {
+    }
+
+    ngOnInit() {
+        this.numberService.mode = 'yourNumbers';
+        this.getUserLines();
+        this.generateRowColumns();
+    }
+
+    getUserLines() {
+        this.numberService.getUserLines(this.tableConfig.pagingModel.pageNumber, this.tableConfig.pagingModel.pageSize, this.getUserLineModel.isActive, this.phrase)
+            .subscribe(res => {
+                this.userLines = res.data.items;
+                console.log(this.userLines);
+                this.tableConfig.pagingModel.totalItemsCount = res.data.totalItemsCount;
+            });
+    }
 
 
-  doPaging(e) {
-    this.getUserLineModel.pageNumber = e;
-    this.getUserLines();
-  }
+    modifyUserLine(data: ItemsUserLineInterface, index) {
+        this.currentIndex = index;
+        this.openDialog('400px', 'auto', '', {data, index});
+    }
 
 
-  modifyUserLine(data: ItemsUserLineInterface, index) {
-    this.currentIndex = index;
-    this.openDialog('400px', 'auto', '', {data, index});
-  }
+    openDialog(width, height, panelClass, data): void {
+        const dialogRef = this.dialog.open(EditUserLinesComponent, {
+            width,
+            height,
+            panelClass,
+            data
+        });
 
+        dialogRef.afterClosed()
+            .subscribe(result => {
+                if (result && result.status == 2) {
+                    this.userLines.splice(this.currentIndex, 1);
+                    this.userLines.push(result.data);
+                    this.notificationService.success('Line extended successfully', '');
+                } else if (result && result.status == 1) {
+                    this.notificationService.success('Payment management modified successfully', '');
+                }
+            });
+    }
 
-  openDialog(width, height, panelClass, data): void {
-    const dialogRef = this.dialog.open(EditUserLinesComponent, {
-      width,
-      height,
-      panelClass,
-      data
-    });
+    getData(event) {
+        this.phrase = event;
+        this.getUserLines();
+    }
 
-    dialogRef.afterClosed()
-      .subscribe(result => {
-        if (result && result.status == 2) {
-          this.userLines.splice(this.currentIndex, 1);
-          this.userLines.push(result.data);
-          this.notificationService.success('Line extended successfully', '');
-        } else if (result && result.status == 1) {
-          this.notificationService.success('Payment management modified successfully', '');
-        }
-      });
-  }
+    generateRowColumns() {
+        this.tableConfig.rowColumnsConfig.push({propertyName: 'countryName'});
+        this.tableConfig.rowColumnsConfig.push({propertyName: "number", sign: '+'});
+        this.tableConfig.rowColumnsConfig.push({propertyName: "creationDateTime", isDateTime:true});
+        this.tableConfig.rowColumnsConfig.push({propertyName: "cost",sign: '€'})
+    }
 
-  getData(event) {
-    this.phrase = event;
-    this.getUserLines();
-  }
 }
